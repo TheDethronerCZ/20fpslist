415f28903919f1aed7a0bf8be753aaad9e19c256
Heraij <brilliant2023.hotmail@gmail.com>
2026-07-22 22:39:54 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,13 +100,13 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Record must be on EXACTLY 20FPS
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        no hax that bad according to pointercrate
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        should have taps or raw footage
                     </p>
                     <p>
                         The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
@@ -121,7 +121,7 @@ export default {
                         Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        if u no follow rule u bad
                     </p>
                 </div>
             </div>
